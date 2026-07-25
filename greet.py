cf917c90c29257d3834aef518f0778ff0915cf24
guavava1506 <a09119131133@gmail.com>
2026-07-25 14:43:53 +0800

    顯示時間

---
+++ greet.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+weekday_map = ["一", "二", "三", "四", "五", "六", "日"]
+
+now = datetime.now(ZoneInfo("Asia/Taipei"))
+weekday = weekday_map[now.weekday()]
+
+print(f"西元{now.year}.{now.month:02d}.{now.day:02d} 星期{weekday}")
